e06afb8c247aa33a7bdfccd9c370a12c3a53ee8f
Aarav Nair <224329735+Aario-Wii@users.noreply.github.com>
2026-08-01 21:21:30 +0530

    File Change: Added Third Party Licenses

--- MethodLibrary/obj/Debug/net9.0/MethodLibrary.AssemblyInfo.cs
+++ MethodLibrary/obj/Debug/net9.0/MethodLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2901b7c0d501309238bf420d979614e9237d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
